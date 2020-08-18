abbfd51377c1641ed825dd9dd6de310561b89b83
JDBennison <jamesbennison88@gmail.com>
2020-08-18 11:08:24 +0000

    Add edit review page.

--- app.py
+++ app.py
@@ -109,6 +109,13 @@ def add_review():
     return render_template("add_review.html", categories=categories)
 
 
+@app.route("/edit_review/<review_id>", methods=["GET", "POST"])
+def edit_review(review_id):
+    review = mongo.db.reviews.find_one({"_id": ObjectId(review_id)})
+
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("edit_review.html", review=review, categories=categories)
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")), debug=True)
